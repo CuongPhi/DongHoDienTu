ad19ee700a7370df7013ce5125dd8bc7e12e8163
nguyen phi cuong <nficuong@gmail.com>
2018-05-31 23:44:09 +0700

    first commit

---
+++ DongHoDienTu/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DongHoDienTu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UCDongHo uc = new UCDongHo();
+
+        Button btnCurrent = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GridUC.Children.Add(uc);
+            uc.setTimeClose += new UCDongHo.SetTimeClose(CloseWindow);
+
+        }
+        void CloseWindow(bool b)
+        {
+            if (b)
+                Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnCurrent != null)
+            {
+                btnCurrent.Background = (Brush)(new BrushConverter()).ConvertFrom("#dddddd");
+            }
+            btnCurrent = btnStart;
+            btnCurrent.Background = Brushes.OrangeRed;
+            uc.Start();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (btnCurrent != null)
+            {
+                btnCurrent.Background = (Brush)(new BrushConverter()).ConvertFrom("#dddddd");
+            }
+            btnCurrent = btnStop;
+            btnCurrent.Background = Brushes.OrangeRed;
+            uc.Stop();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (btnCurrent != null)
+            {
+                btnCurrent.Background = (Brush)(new BrushConverter()).ConvertFrom("#dddddd");
+            }
+            btnCurrent = btnResu;
+            btnCurrent.Background = Brushes.OrangeRed;
+            uc.Resume();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (btnCurrent != null)
+            {
+                btnCurrent.Background = (Brush)(new BrushConverter()).ConvertFrom("#dddddd");
+            }
+            btnCurrent = btnPause;
+            btnCurrent.Background = Brushes.OrangeRed;
+            uc.Pause();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            //bool flag = false;
+            //if( btnCurrent != null )
+            //{
+            //    if(btnCurrent.Name == "btnStop")
+            //         flag = true;            
+            //}
+            //else
+            //{
+            //    flag = true;
+              
+            //}
+            //if (flag)
+            //{
+                int seconds = int.Parse(((ComboBoxItem)(cbbMinu2.SelectedItem)).Content.ToString()) +
+                            int.Parse(((ComboBoxItem)(cbbMinu1.SelectedItem)).Content.ToString()) * 10;
+                int minutes = int.Parse(((ComboBoxItem)(cbbSec2.SelectedItem)).Content.ToString()) +
+                    int.Parse(((ComboBoxItem)(cbbSec1.SelectedItem)).Content.ToString()) * 10;
+                uc.Close(minutes, seconds);
+            lbSetTimeClose.Content = string.Format("Thoát {0}:{1}",minutes,seconds);
+            lbSetTimeClose.Foreground = Brushes.Red;
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Cài giờ tắt khi đồng hồ không chạy !");
+            //}
+        }
+
+
+    }
+}
---
+++ DongHoDienTu/UCDongHo.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DongHoDienTu
+{
+    /// <summary>
+    /// Interaction logic for UCDongHo.xaml
+    /// </summary>
+    public partial class UCDongHo : UserControl
+    {
+        private Status sst = Status.Stop;
+        System.Windows.Threading.DispatcherTimer dispatcherTimer;
+        private int seconds = 0;
+        private int minutes = 0;
+        private int sec_close = 99;
+        private int min_close = 99;
+        public delegate void SetTimeClose(bool b);
+        public SetTimeClose setTimeClose;
+        public UCDongHo()
+        {
+            InitializeComponent();
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            SetDefault();
+
+        }
+
+        public void Close(int m, int s)
+        {
+            sec_close = s;
+            min_close =m;           
+        }
+        void SetDefault()
+        {
+            SetImage(_0, "0");
+            SetImage(_1, "0");
+            SetImage(_2, "-");
+            SetImage(_3, "0");
+            SetImage(_4, "0");
+        }
+        public void Pause()
+        {
+            if (sst == Status.Start)
+            {
+                dispatcherTimer.Stop();
+                sst = Status.Pause;
+            }
+        }
+        public void Resume()
+        {
+            if (sst == Status.Pause)
+            {
+                dispatcherTimer.Start();
+                sst = Status.Start;
+            }
+        }
+        public void Stop()
+        {
+            sst = Status.Stop;
+            SetDefault();
+            seconds = minutes = 0;
+            dispatcherTimer.Stop();
+        }
+        public void Start()
+        {
+            sst = Status.Start;
+            seconds = minutes = 0;
+            dispatcherTimer.Start();
+        }
+        void SetImage(Image im, string path)
+        {
+            im.Source = new BitmapImage(new Uri("pack://application:,,,/DongHoDienTu;component/" + path+".png"));
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            if (minutes == min_close && sec_close == seconds)
+            {
+                setTimeClose(true);
+            }
+            if (minutes == 59 && seconds == 60)
+            {
+                dispatcherTimer.Stop();
+                return;
+            }
+            if (seconds % 60 == 0)
+            {
+                minutes++;
+            }
+            if (minutes % 10==0)
+            {
+                SetImage(_0, (minutes / 10).ToString());
+
+            }
+            SetImage(_1, (minutes % 10).ToString());
+            if (seconds == 60)
+            {
+                seconds = 0;
+            }
+            if (seconds % 10==0)
+            {
+                SetImage(_3, (seconds / 10).ToString());              
+
+            }
+            SetImage(_4, (seconds % 10).ToString());
+            
+
+        }
+    }
+    public enum Status
+    {
+        Start, Stop, Pause
+    }
+}
